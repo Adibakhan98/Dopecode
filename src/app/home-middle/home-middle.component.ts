c11d0745e14de27b00a14e2c48257f3cf5614de9
Adibakhan98 <90059448+Adibakhan98@users.noreply.github.com>
2021-09-10 21:35:27 +0530

    added text

--- src/app/home-middle/home-middle.component.ts
+++ src/app/home-middle/home-middle.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit  } from '@angular/core';
+declare var anime: any; 
 
 @Component({
   selector: 'app-home-middle',
@@ -10,6 +11,26 @@ export class HomeMiddleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+ ngAfterViewInit(): void {
+ 
+anime.timeline({loop: true})
+  .add({
+    targets: '.c2 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.c2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
   }
 
 }
